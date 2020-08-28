4b2e1674aafd59a05431f20ce283a55bb5bc942c
Piotr Esden-Tempski <piotr@esden.net>
2020-08-27 21:46:08 -0700

    Added UDFN-14_1.35x3.5mm_Pitch0.5mm

--- cadquery/FCAD_script_generator/QFN_packages/cq_parameters.py
+++ cadquery/FCAD_script_generator/QFN_packages/cq_parameters.py
@@ -776,6 +776,32 @@ kicad_naming_params_qfn = {
         rotation = -90, # rotation if required
         dest_dir_prefix = '../Housings_DFN_QFN.3dshapes/'
         ),
+    'UDFN-14_1.35x3.5mm_Pitch0.5mm': Params( # from https://www.littelfuse.com/~/media/electronics/datasheets/tvs_diode_arrays/littelfuse_tvs_diode_array_sp3012_datasheet.pdf.pdf
+        c = 0.2,        # pin thickness, body center part height
+#        K=0.2,          # Fillet radius for pin edges
+        L = 0.3,        # pin top flat part length (including fillet radius)
+        fp_s = True,     # True for circular pinmark, False for square pinmark (useful for diodes)
+        fp_r = 0.4,     # first pin indicator radius
+        fp_d = 0.04,     # first pin indicator distance from edge
+        fp_z = 0.01,     # first pin indicator depth
+        ef = 0.0, # 0.05,      # fillet of edges  Note: bigger bytes model with fillet
+        cce = 0.2,      #0.45 chamfer of the epad 1st pin corner
+        D = 3.5,       # body overall length
+        E = 1.35,       # body overall width
+        A1 = 0.02,  # body-board separation  maui to check
+        A2 = 0.5,  # body height
+        b = 0.2,  # pin width
+        e = 0.5,  # pin (center-to-center) distance
+        m = 0.0,  # margin between pins and body
+        ps = 'square',   # square pads
+        npx = 7,  # number of pins along X axis (width)
+        npy = 0,  # number of pins along y axis (length)
+        epad = None, # e Pad #epad = None, # e Pad
+        excluded_pins = None, #no pin excluded
+        modelName = 'UDFN-14_1.35x3.5mm_Pitch0.5mm', #modelName
+        rotation = -90, # rotation if required
+        dest_dir_prefix = '../Housings_DFN_QFN.3dshapes/'
+        ),
     'DFN-4-1EP_1x1mm_P0.65mm_EP0.5x0.5mm': Params( # from http://ww1.microchip.com/downloads/en/DeviceDoc/MIC550x-300mA-Single-Output-LDO-in-Small-Packages-DS20006006A.pdf
         c = 0.025,      # pin thickness, body center part height
 #        K=0.2,         # Fillet radius for pin edges
